9755fd89e0c94d58cce03d4ffaec46ab40d38819
Jinu Thomas <jimalhere@gmail.com>
2014-05-29 19:03:43 +0530

    modified:   Gemfile 	modified:   Gemfile.lock 	modified:   app/assets/images/bg-company-contact.jpg 	modified:   app/assets/images/categories/mobility.jpg 	modified:   app/assets/images/favicon.ico 	modified:   app/assets/images/footer-logo.png 	modified:   app/assets/images/footer-logo@2x.png 	modified:   app/assets/images/home-hero.jpg 	modified:   app/assets/images/how-it-works.jpg 	modified:   app/assets/images/icons/dollar-heart.png 	modified:   app/assets/images/logo-icon-large.png 	modified:   app/assets/images/logo-icon.png 	modified:   app/assets/images/logo.png 	modified:   app/assets/images/logo@2x.png 	modified:   app/assets/images/markitup/h3.png 	modified:   app/assets/images/markitup/h4.png 	modified:   app/assets/images/markitup/h5.png 	modified:   app/assets/images/markitup/h6.png 	modified:   app/assets/images/markitup/italic.png         modified:   app/assets/images/markitup/link.png 	modified:   app/assets/images/markitup/list-bullet.png 	modified:   app/assets/images/markitup/list-numeric.png 	modified:   app/assets/images/markitup/picture.png 	modified:   app/assets/images/markitup/quotes.png         modified:   app/assets/images/neighborly_ograph_logo.jpg 	modified:   app/assets/images/pin.png 	modified:   app/assets/images/start/bg_launch.png 	modified:   app/assets/images/start/bg_plan.png 	modified:   app/assets/images/start/bg_review.png 	modified:   app/assets/images/start/bg_submit.png 	modified:   app/assets/images/start/icon_step1.png 	modified:   app/assets/images/start/icon_step2.png 	modified:   app/assets/images/start/icon_step3.png 	modified:   app/assets/images/start/icon_step4.png 	modified:   app/assets/stylesheets/modules/hero-header.sass 	modified:   app/assets/stylesheets/modules/hero-header.sass~ 	modified:   app/locales/modules/footer.en.yml 	modified:   app/locales/modules/footer.en.yml~ 	modified:   app/locales/modules/header.en.yml 	modified:   app/locales/pages/projects/index.en.yml 	modified:   app/locales/pages/projects/index.en.yml~ 	modified:   app/views/custom/static/about.html.slim 	modified:   app/views/custom/static/how_it_works.html.slim 	modified:   app/views/custom/static/privacy.html.slim 	modified:   app/views/custom/static/terms.html.slim         modified:   app/views/layouts/_footer.html.slim 	modified:   app/views/layouts/_footer.html.slim~ 	modified:   app/views/layouts/email.html.slim 	modified:   config/database.yml~ 	modified:   db/seeds.rb 	modified:   db/seeds.rb~
     Untracked files:
       (use "git add <file>..." to include in what will be committed)
    
    	Gemfile~
    	app/assets/images/header_system.png
    	app/assets/images/hero_new.jpg
    	app/locales/modules/header.en.yml~
    	app/views/custom/static/about.html.slim~
    	app/views/custom/static/how_it_works.html.slim~
    	app/views/custom/static/privacy.html.slim~
    	app/views/custom/static/terms.html.slim~
    	app/views/layouts/application.html.slim~
    	app/views/layouts/email.html.slim~

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,14 +3,19 @@
 puts 'Seeding the database...'
 
 [
+  { pt: 'Charity Activities', en: 'Charity Activities' },
   { pt: 'construções', en: 'Constructions' },
-  { pt: 'assistência médica', en: 'Medical Aid' },
-  { pt: 'sem casa', en: 'Homeless' },
-  { pt: 'educação', en: 'Education' },
   { pt: 'pão de cada dia', en: 'Daily Bread' },
+  { pt: 'educação', en: 'Education' },
+  { pt: 'Handicapped', en: 'Handicapped' },
+  { pt: 'sem casa', en: 'Homeless' },
   { pt: 'endividado', en: 'Indebted' },
+  { pt: 'Jobless', en: 'Jobless' },
+  { pt: 'Livelihood', en: 'Livelihood' },
+  { pt: 'assistência médica', en: 'Medical Aid' },
+  { pt: 'Public Welfare', en: 'Public Welfare' },
   { pt: 'religioso', en: 'Religious' },
-  { pt: 'outros', en: 'Others' }
+  { pt: 'others', en: 'Others' }
 ].each do |name|
    category = Category.find_or_initialize_by(name_pt: name[:pt])
    category.update_attributes({
@@ -19,34 +24,47 @@ puts 'Seeding the database...'
  end
 
 {
-  company_name: 'Silver Alms',
-  company_logo: 'http://silveralms.com/assets/catarse_bootstrap/logo_icon_catarse.png',
-  host: 'silveralms.com',
+  company_name: 'SilverAlms',
+  company_logo: 'http://silveralms.com/assets/logo@2x.png',
+  host: 'silveralms',
   base_url: "http://silveralms.com",
 
-  email_contact: 'contact@silveralms.com',
-  email_payments: 'finance@silveralms.com',
-  email_projects: 'project@silveralms.com',
-  email_system: 'system@silveralms.com',
-  email_no_reply: 'no-reply@silveralms.com',
-  facebook_url: "http://www.facebook.com/pages/SilverAlmsCom/1432743666987583",
+  email_contact: 'contact@support.silveralms.com',
+  email_payments: 'payments@support.silveralms.com',
+  email_projects: 'projects@support.silveralms.com',
+  email_system: 'system@support.silveralms.com',
+  email_no_reply: 'no-reply@support.silveralms.com',
+  facebook_url: "https://www.facebook.com/pages/SilverAlmsCom/1432743666987583",
   facebook_app_id: '686963018037797',
   twitter_url: 'http://twitter.com/silveralms',
   twitter_username: "silveralms",
   mailchimp_url: "http://ownmanager.us8.list-manage1.com/subscribe?u=9f171110a102fc24feed06f24&id=1df03ad4dd",
+  mailchimp_api_key: '0fc042d8ac466115a48e779e04770258-us8',
+  mailchimp_list_id: 'mailchimp_list_id_sample',
+  mailchimp_headline_list_url: 'mailchimp_headline_list_url_sample',
+  mandrill_user_name: 'mandrill_user_name_sample',
+  mandrill: 'mandrill_sample',
+  paypal_username: 'jtmsss_api1.yahoo.com',
+  paypal_password: 'FCUBSHJJ9HBWTY2L',
+  paypal_signature: 'A.A-8UOGDek6uyXy2N.a3h8WjyuHAaDBxo0OjvTdG5.1IMGIyaoxHJJf',
+  authorizenet_login_id: 'authorizenet_login_id_sample',
+  authorizenet_transaction_key: 'authorizenet_transaction_key_sample',
   catarse_fee: '0.10',
-  support_forum: 'http://support.silveralms.com',
+  support_forum: 'http://suport.silveralms.com',
   base_domain: 'silveralms.com',
   uservoice_secret_gadget: 't4YVh1DqPV4t99sFoAUv9wL2XY137LRjbD6gfyN0CtA',
   uservoice_key: 'CFL6GwuDuCmFpfAQhe8AA',
-  faq_url: 'http://support.silveralms.com',
-  feedback_url: 'http://support.silveralms.com/feedback',
-  terms_url: 'http://support.silveralms.com/terms',
-  privacy_url: 'http://support.silveralms.com/privacy',
-  about_channel_url: 'http://support.silveralms.com/aboutchannel',
+  aws_bucket: 'aws_bucket_sample',
+  aws_access_key: 'aws_access_key_sample',
+  aws_secret_key: 'aws_secret_key_sample',
+  faq_url: 'http://silveralms.com/faq',
+  feedback_url: 'http://support.silveralms.com',
+  terms_url: 'http://silveralms.com/terms',
+  privacy_url: 'http://silveralms.com/privacy',
+  about_channel_url: 'http://silveralms.com/about',
   instagram_url: 'http://instagram.com/silveralms',
   blog_url: "http://blog.silveralms.com",
-  github_url: 'http://github.com/jinutm/silveralms.git',
+  github_url: 'http://github.com/jinutm',
   contato_url: 'http://support.silveralms.com'
 }.each do |name, value|
    conf = Configuration.find_or_initialize_by(name: name)
@@ -57,20 +75,20 @@ end
 
 
 OauthProvider.find_or_create_by!(name: 'facebook') do |o|
-  o.key = '686963018037797'
-  o.secret = 'aafec836b6d5493ce8ab61af0d63d5a1'
+  o.key = '245932138941468'
+  o.secret = '192c6ff9d43347513637eb06d1b40b4c'
   o.path = 'facebook'
 end
 
 OauthProvider.find_or_create_by!(name: 'twitter') do |o|
-  o.key = 'your_twitter_app_key'
-  o.secret = 'your_twitter_app_secret'
+  o.key = '8aalVWQE4ySVisDNSJborpOfA'
+  o.secret = 'hfSrM52SKLNXSVOremn9SYrXUJ56vJVrwKKnN0EmkipxAgMuKo'
   o.path = 'twitter'
 end
 
 OauthProvider.find_or_create_by!(name: 'google_oauth2') do |o|
-  o.key = 'your_google_oauth2_app_key'
-  o.secret = 'your_google_oauth2_app_secret'
+  o.key = 'AIzaSyAJvy1sO-M3dfGjTjj3DgytxIqkdFD6PAU'
+  o.secret = 'us3S7QJWjvCy85doErOYHzlG'
   o.path = 'google_oauth2'
 end
 
@@ -107,3 +125,16 @@ end
 
 puts '---------------------------------------------'
 puts 'Done!'
+
+
+
+
+# Neighbor.ly
+
+# User to receive company contact notifications
+contact_user = User.create! email: Configuration[:email_contact], password: SecureRandom.hex(4)
+contact_user.confirm! if contact_user
+
+# User to receive new projects on draft notifications
+project_user = User.create! email: Configuration[:email_projects], password: SecureRandom.hex(4)
+project_user.confirm! if project_user
